442a4975bffb65eaa3b19b3bfc68a1c3eaa54d03
sjxiang <1535484943@qq.com>
2022-09-08 17:56:12 +0800

    updaet signup handler

--- dao/mysql/user.go
+++ dao/mysql/user.go
@@ -3,8 +3,8 @@ package mysql
 import (
 	"errors"
 
-	// "github.com/sjxiang/bluebell/models"
-	// "golang.org/x/crypto/bcrypt"
+	"github.com/sjxiang/bluebell/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // 把每一步数据库操作封装成函数
@@ -15,7 +15,7 @@ func CheckUserExist(username string) (err error) {
 
 	// var users []models.User
 	// DB.Model(user).Select("user_id").Where("username = ?", username).Count(&count)
-	
+
 	var count int64
 
 	err = DB.Table("user").Select("user_id").Where("username = ?", username).Count(&count).Error
@@ -32,21 +32,27 @@ func CheckUserExist(username string) (err error) {
 }
 
 
-// // InsertUser 向数据库中插入一条新的记录
-// func InsertUser(user *models.User) (err error) {
-// 	// 对密码加密
+// InsertUser 向数据库中插入一条新的记录
+func InsertUser(user *models.User) (err error) {
+
+	// 对密码加密
 	
-// 	// GenerateFromPassword 的第二个参数时 cost 值，建议大于 12，数值越大耗费时间越长
-// 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
-// 	if err != nil {
-// 		return
-// 	}
-	
-// 	// 执行 SQL 语句入库
-// 	sqlStr := `insert into user(user_id, username, password) values(?,?,?)`
-// 	_, err = db.Exec(sqlStr, user.UserID, user.Username, string(hash))
-// 	return 
-// }
+	// GenerateFromPassword 的第二个参数时 cost 值，建议大于 12，数值越大耗费时间越长
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return
+	}
+
+	user.Password = string(hash)
+
+	// 执行 SQL 入库
+	result := DB.Create(user)
+	if result.Error != nil {
+		return 
+	}
+
+	return nil  
+}
 
 
 // func QueryUserByUsername() {
--- logic/user.go
+++ logic/user.go
@@ -2,8 +2,8 @@ package logic
 
 import (
 	"github.com/sjxiang/bluebell/dao/mysql"
-	// "github.com/sjxiang/bluebell/models"
-	// "github.com/sjxiang/bluebell/pkg/snowflake"
+	"github.com/sjxiang/bluebell/models"
+	"github.com/sjxiang/bluebell/pkg/snowflake"
 	"github.com/sjxiang/bluebell/requests"
 )
 
@@ -18,18 +18,18 @@ func Signup(p *requests.ParamSignup) (err error) {
 	}
 
 	// 2. 生成 uid
-	// userID := snowflake.GetID()
+	userID := snowflake.GetID()
 	
 	// 构造 1 个 user 实例
-	// u := &models.User{
-	// 	UserID: userID,
-	// 	Username: p.Username,
-	// 	Password: p.Password,
-	// }
+	u := models.User{
+		UserID: userID,
+		Username: p.Username,
+		Password: p.Password,
+	}
 
 
 	// 3. 保存进数据库
-	// err = mysql.InsertUser(u)
+	err = mysql.InsertUser(&u)
 
 	return
 }
